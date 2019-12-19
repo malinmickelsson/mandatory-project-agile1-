49126ef36a1401d580f88b7edd31c881399f9d52
Flashkeys <43600429+Flashkeys@users.noreply.github.com>
2019-12-19 17:32:34 +0100

    temp save

---
+++ frontend/src/components/Global/Chatt.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from "@reach/router";
+import '../../css/home.css';
+
+const Chatt = () => {
+
+
+  return (
+    <div className="main-chatt-container">
+
+      <div className="chatt-container">
+
+        <div className="chatt-text-container">
+          <div className="chatt-user">
+            <span>User : </span>
+            <span>Här kommer texten att vara</span>
+          </div>
+
+          <div className="chatt-me">
+            <span>Här kommer din text</span>
+          </div>
+
+        </div>
+        <br />
+        <div className="chatt-input-container">
+          <input type="text" />
+          <button>Send</button>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+export default Chatt
