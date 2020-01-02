20b1e93b8e201e670bf6e6a6d160eb9201bdb356
Flashkeys <43600429+Flashkeys@users.noreply.github.com>
2020-01-02 19:39:06 +0100

    new game popup

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from "@reach/router";
 import './css/home.css';
 import Popup from "./components/Global/Popup.jsx";
@@ -6,13 +6,18 @@ import Chatt from "./components/Global/Chatt.jsx";
 
 const Home = () => {
 
-    
+    const [newGame, setNewGame] = useState(false);
+
+    function popupNewGame () {
+        setNewGame(true);
+    }
 
     return (
         <div className="container">
             <h1>Home page</h1>
             <div className="new-game">
-                <Popup page="home" />
+                {/* <Popup page="home" /> */}
+                <button onClick={popupNewGame} > New Game</button>
             </div>
             <nav>
                 <Link to="/">Home</Link> <Link to="dashboard">Dashboard</Link>
@@ -22,6 +27,9 @@ const Home = () => {
             <br />
             <br />
             <Chatt />
+            {newGame ?
+            <Popup page="temp" setNewGame={setNewGame} />
+            : null}
         </div>
     );
 }
--- frontend/src/components/Global/Popup.jsx
+++ frontend/src/components/Global/Popup.jsx
@@ -1,25 +1,83 @@
 import React, { useState } from 'react';
+import Home from "../../Home.jsx";
 //import { Link } from "@reach/router";
-// import './css/home.css';
+import './popup.css';
 
 const Popup = (props) => {
 
-    const [modal, setModal] = useState(false);
-    const [modalCancel, setModalCancel] = useState(false);
+  const [modal, setModal] = useState(false);
+  const [modalCancel, setModalCancel] = useState(false);
 
+  function closeModal() {
+    setModalCancel(true);
+    props.setNewGame(false);
+  }
 
-    return (
-        <div className="container">
+  return (
+    <div className="container">
 
-            {props.page === "home" ?
-                <div className="popup-home-container">
+      <div className="modal">
+        {props.page === "temp" ?
+          <div className="modal-content">
+            <div className="modal-header">
+              <button onClick={closeModal} className="close-btn">&times;</button>
+            </div>
+            <div className="modal-body">
+              <br />  
+              <section>
+                <span>Choose color : </span>
+              </section>
+    
+              <section>
+                <span>black</span><input className="color-black" type="radio" />
+                <span>white</span><input className="color-white" type="radio" />
+              </section>
 
-                </div>
-                :
-                null
-            }
+              <br />
+
+              <section>
+                <span>Game name</span>
+              </section>
+              <section>
+                <input type="text" />
+              </section>
+
+              <br />
+
+              <section>
+                <span>Time</span>
+              </section>
+              <section>
+                <input type="number" placeholder="min..." />
+              </section>
+              
+              <br />
+
+            <section>
+              <button>Creat Game</button>
+            </section>
+            <br />
+
+            </div>
+            <br />
+            <div className="modal-footer">
+              <span>this will be the footer</span>
+            </div>
+
+          </div>
+          : null}
+
+      </div>
+
+      {props.page === "home" ?
+        <div className="popup-home-container">
 
         </div>
-    );
+        :
+        null
+      }
+
+    </div>
+  );
 }
 export default Popup
