e39ada74af58e1fc3e40f1132ece7231ba77ba30
Radoslava Tsankova Todorova <radoslavatsankovatodorova@MacBook-de-Radoslava.local>
2020-01-03 17:26:31 +0100

    GameStatusHub Finished #51.
    Skapade frontend för spel-vyn i ChessBoard-page. Första Status-game design.

--- frontend/src/components/Gameboard/GameStatusHub.jsx
+++ frontend/src/components/Gameboard/GameStatusHub.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
 
+import {
+  StatusList, Wrapper, OppTime, OppTur, YourTime,
+  YourTur, Txt, ChattInput, ChattButton, TurnBox, Ul, Li
+} from './styles';
+
 const GameStatusHub = () => {
+
   return (
-    <div>
-      
-    </div>
+    <StatusList>
+      <Wrapper>
+        <Txt>Opponent Time</Txt>
+        <OppTime>10:10</OppTime>
+        <OppTur></OppTur>
+      </Wrapper>
+      <Wrapper>
+        <Txt>Drag-View</Txt>
+      </Wrapper>
+      <Wrapper>
+        <Txt>Your Time</Txt>
+        <YourTime>10:10</YourTime>
+        <YourTur>Your Turn</YourTur>
+      </Wrapper>
+      <Txt>Chat-View</Txt>
+      <Ul>
+        <Li>Your text..</Li>
+        <Li>Opponent text..</Li>
+      </Ul>
+      <TurnBox>
+        <ChattInput type="text" placeholder=" Write a message .."></ChattInput>
+        <ChattButton>Send</ChattButton>
+      </TurnBox>
+    </StatusList>
   )
 }
 
-export default GameStatusHub
+export default GameStatusHub;
