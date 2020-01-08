c3400afe844b9f2b431d85198fbc59abb1a4c064
Malin Larsson <malin.c.larsson@me.com>
2020-01-08 16:06:10 +0100

    working on chat

--- frontend/src/components/Global/Chatt.jsx
+++ frontend/src/components/Global/Chatt.jsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 //import { Link } from "@reach/router";
 import { ChattText, ChattInput, ChattButton, TurnBox } from "./style";  // ChattMe, ChattUser,
 import io from 'socket.io-client'
 
+
+//=============== chatten ===============//
+export default class Chatt extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+      error: ""
+    };
+  }
+
+
 //=============== socket ===============//
-const chatFunction = () => {
+function () {
   let socket = io();
   ('form').submit(function(e) {
     e.preventDefault();
@@ -14,24 +27,37 @@ const chatFunction = () => {
   })
 }
 
-//=============== chatten ===============//
-const Chatt = () => {
-  return (
-    <div>
+  
+  render() { // eslint-disable-next-line 
+    const { message, error } = this.state;
+
+
+    return (
+    <form onSubmit={this.handleSubmit}>
+
+      <ChattText> hej hej! *vinkar* </ChattText>
+
+      <TurnBox>
+        <ChattInput  
+          ref={input => {
+              this.textInput = input;
+            }}
+            type="text"
+            id="message"
+            value= 'message' //{message}
+            onChange={this.handleChange}
+            placeholder={"..."}>
+        </ChattInput>
+
+        <ChattButton type="submit" >Send</ChattButton>
+      </TurnBox>
+    </form>
+  );
+};
+}
+
+      // eslint-disable-next-line
       {/* <ChattMe> Min username :</ChattMe> 
         <ChattText> här kommer meddelandet </ChattText>
       <ChattUser> Min username :</ChattUser> 
         <ChattText> här kommer meddelandet </ChattText> */}
-
-      <ChattText> { chatFunction } </ChattText> */}
-
-      <TurnBox>
-        <ChattInput type="text" placeholder=" Write a message .."></ChattInput>
-        <ChattButton>Send</ChattButton>
-      </TurnBox>
-    </div>
-  );
-};
-
-export default Chatt;
-
